4d242885f05656e2f9781a14a185f5aacccb6fa1
Rodrigo Ramirez <rodrigo.ramirez@softvision.com>
2020-08-16 21:51:57 -0300

    testing CI/CD

---
+++ src/Ecommerce.API/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>() { new Product() { ProductId = 1, Name = "Product name", Description = "Description" } };
+        }
+    }
+}
---
+++ src/domain/Ecommerce.Domain.Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Model
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
